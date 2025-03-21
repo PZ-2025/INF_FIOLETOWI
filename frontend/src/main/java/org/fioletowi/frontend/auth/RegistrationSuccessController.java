522d4cc51a411d2b7c4c4efb708d4faf9e6733db
XTheAla <132890984+XTheAla@users.noreply.github.com>
2025-03-21 19:45:04 +0100

    RegistrationSuccessControl

---
+++ frontend/src/main/java/org/fioletowi/frontend/auth/RegistrationSuccessController.java
@@ -0,0 +1,42 @@
+package com.example.farm_registration.auth;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+
+import java.io.IOException;
+
+public class RegistrationSuccessController {
+
+    @FXML
+    private Text user_email;
+
+    /*@FXML
+    public void initialize() {
+        user_email.setText("[email@gmail.com]");
+    }*/
+    public void setUserEmail(String email) {
+        user_email.setText(email);
+    }
+
+    @FXML
+    private void backToLoginOnAction(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/farm_registration/login.fxml"));
+            Parent loginView = loader.load();
+
+            Scene currentScene = ((Node) event.getSource()).getScene();
+            currentScene.setRoot(loginView);
+
+            Stage stage = (Stage) currentScene.getWindow();
+            stage.setTitle("Login");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
