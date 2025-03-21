2f728b60029092d1bae259eede3eb7e2e815e690
nali <119813729+dazedoz@users.noreply.github.com>
2025-03-21 20:18:44 +0100

    Update Main.java

--- frontend/src/main/java/org/fioletowi/frontend/Main.java
+++ frontend/src/main/java/org/fioletowi/frontend/Main.java
@@ -10,17 +10,24 @@ import java.util.prefs.Preferences;
 
 public class Main extends Application {
 
+    public static Preferences getPreferences() {
+        return Preferences.userNodeForPackage(Main.class);
+    }
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("login.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 400);
 
         // Sprawdzamy, czy użytkownik ma Dark Mode włączony
-        Preferences preferences = Preferences.userNodeForPackage(Main.class);
-        boolean isDarkMode = preferences.getBoolean("dark_mode", false); // Domyślnie OFF
+        Preferences preferences = getPreferences();
+        boolean isDarkMode = preferences.getBoolean("dark_mode", false); // Domyślnie OF
 
         if (isDarkMode) {
             scene.getStylesheets().add(getClass().getResource("/com/example/styles/dark-mode.css").toExternalForm());
+        }
+        else {
+            scene.getStylesheets().add(getClass().getResource("/com/example/styles/light-mode.css").toExternalForm());
         }
 
         stage.setScene(scene);
